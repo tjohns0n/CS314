803f5db3ad091e25c6d26d46623a4a889d099c8c
tjohns0n <tjohns0n@rams.colostate.edu>
2017-02-13 19:13:07 -0700

    Simplified representation of XML elements Adds tests for XMLElement closes #65, closes #66, closes #68

--- src/View/SVGWriter.java
+++ src/View/SVGWriter.java
@@ -8,7 +8,7 @@ package View;
 public class SVGWriter {
 
     // Width and height of the SVG. Currently hardcoded to match the sample SVG on GitHub
-    int width = 1200;
+    int width = 1280;
     int height = 1024;
     // Trip to be drawn
     // Trip trip;
