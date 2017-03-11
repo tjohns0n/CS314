25187b0f1856e806325a8efd770a24a44b0e80d7
tjohns0n <tjohns0n@rams.colostate.edu>
2017-03-10 20:04:53 -0700

    fixes #168

--- src/test/java/edu/csu2017sp314/DTR14/tripco/Model/ModelTest.java
+++ src/test/java/edu/csu2017sp314/DTR14/tripco/Model/ModelTest.java
@@ -1,33 +1,42 @@
 package edu.csu2017sp314.DTR14.tripco.Model;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 import org.junit.*;
 
 public class ModelTest{
 
-	private final static String fileName = "Colorado14ers.csv";
-
 	@Test
 	public void testConstructor() {
-		Model model = new Model(fileName);
+		Model model = new Model("test.csv");
 	}
 
 	@Test
 	public void testPlanTrip() {
-		Model model = new Model(fileName);
-		//assertTrue("wrong trip", model.planTrip());
+		Model model = new Model("test.csv");
+		assertTrue("wrong trip", model.planTrip(false, false));
 	}
 
-        /*
 	@Test
-	public void testReteriveTrip() {
-		Model model = new Model(fileName);
-		model.planTrip();
+	public void testReteriveTrip() throws IOException {
+            try (BufferedWriter b = new BufferedWriter(new FileWriter("test.csv"))) {
+                b.write("name,id,Elevation,Estimated Prominence,latitude,longitude,Quadrangle,Range\n");
+                b.write("Mount Elbert,1,14433,9093,39.1177,-106.4453,Mount Elbert,Sawatch\n");
+                b.write("Mount Massive,2,14421,1961,39.1875,-106.4756,Mount Massive,Sawatch\n");
+                b.write("Mount Harvard,3,14420,2360,38.9243,-106.3208,Mount Harvard,Sawatch\n");
+                b.write("Blanca Peak,4,14345,5325,37.5774,-105.4857,Blanca Peak,Sangre de Cristo\n");
+                b.close();
+            }
+		Model model = new Model("test.csv");
+		model.planTrip(false, false);
 		String tests[][] = model.reteriveTrip();
 		assertTrue("wrong trip", tests[0][0].equals("0,Mount Elbert,39.1177,-106.4453"));
 		assertTrue("wrong trip", tests[1][1].equals("2,14421,1961,Mount Massive,Sawatch"));
 		assertTrue("wrong trip", tests[2][2].equals("id,Elevation,Estimated Prominence,Quadrangle,Range"));
 	}
-*/
 }
--- src/test/java/edu/csu2017sp314/DTR14/tripco/Presenter/PresenterTest.java
+++ src/test/java/edu/csu2017sp314/DTR14/tripco/Presenter/PresenterTest.java
@@ -1,6 +1,9 @@
 package edu.csu2017sp314.DTR14.tripco.Presenter;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
@@ -12,25 +15,36 @@ public class PresenterTest {
 	// TODO add tests
 	private Presenter pres; 
 
-        /*
+        
 	@Test
-	public void testFiles(){
+	public void testFiles() throws IOException{
             ArrayList<File> f = new ArrayList<File>();
-            String pwd = System.getProperty("user.dir");
-            System.out.println(pwd);
-            String testFile = pwd+"/src/Presenter/Testfile.csv";
-            f.add(new File(testFile));
+            try (BufferedWriter b = new BufferedWriter(new FileWriter("test.csv"))) {
+                b.write("name,id,Elevation,Estimated Prominence,latitude,longitude,Quadrangle,Range\n");
+                b.write("Mount Elbert,1,14433,9093,39.1177,-106.4453,Mount Elbert,Sawatch\n");
+                b.write("Mount Massive,2,14421,1961,39.1875,-106.4756,Mount Massive,Sawatch\n");
+                b.write("Mount Harvard,3,14420,2360,38.9243,-106.3208,Mount Harvard,Sawatch\n");
+                b.write("Blanca Peak,4,14345,5325,37.5774,-105.4857,Blanca Peak,Sangre de Cristo\n");
+                b.close();
+            }
+
+            f.add(new File("test.csv"));
             pres = new Presenter(f,9999);
             System.out.println(pres.inFiles.get(0).getAbsolutePath());
 	}
 	
 	@Test
-	public void testRun(){
+	public void testRun() throws IOException{
             ArrayList<File> f = new ArrayList<File>();
-		String pwd = System.getProperty("user.dir");
-		System.out.println(pwd);
-		String testFile = pwd+"/src/Presenter/Testfile.csv";
-		f.add(new File(testFile));
+            try (BufferedWriter b = new BufferedWriter(new FileWriter("test.csv"))) {
+                b.write("name,id,Elevation,Estimated Prominence,latitude,longitude,Quadrangle,Range\n");
+                b.write("Mount Elbert,1,14433,9093,39.1177,-106.4453,Mount Elbert,Sawatch\n");
+                b.write("Mount Massive,2,14421,1961,39.1875,-106.4756,Mount Massive,Sawatch\n");
+                b.write("Mount Harvard,3,14420,2360,38.9243,-106.3208,Mount Harvard,Sawatch\n");
+                b.write("Blanca Peak,4,14345,5325,37.5774,-105.4857,Blanca Peak,Sangre de Cristo\n");
+                b.close();
+            }
+		f.add(new File("test.csv"));
 		pres = new Presenter(f,9999);
 		try{
 			pres.run();
@@ -41,7 +55,7 @@ public class PresenterTest {
 			Assert.fail();
 		}
 	}
-        */
+        
 	
 	//TODO
 	//LOOK HERE
