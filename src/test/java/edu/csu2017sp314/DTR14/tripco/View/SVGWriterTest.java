b7f9d38fa4d74c3eca93806d0ea6bb9b0ad46e7a
bcgdwn <robot221@gmail.com>
2017-03-10 17:12:26 -0700

    Updated tests
    Updated test to reflect changes of last commit

--- src/test/java/edu/csu2017sp314/DTR14/tripco/View/SVGWriterTest.java
+++ src/test/java/edu/csu2017sp314/DTR14/tripco/View/SVGWriterTest.java
@@ -90,18 +90,18 @@ public class SVGWriterTest {
 	public void testAddTitle() {
 		SVGWriter s = new SVGWriter(100, 100);
 		s.addTitle("Test Title", "testid");
-		assertTrue(s.header.get(2).contains("font-size=\"24"));
-		assertTrue(s.header.get(2).contains(">Test Title<"));
-		assertTrue(s.header.get(2).contains("id=\"testid"));
+		assertTrue(s.footer.get(0).contains("font-size=\"24"));
+		assertTrue(s.footer.get(0).contains(">Test Title<"));
+		assertTrue(s.footer.get(0).contains("id=\"testid"));
 	}
 	
 	@Test
 	public void testAddFooter() {
 		SVGWriter s = new SVGWriter(100, 100);
 		s.addFooter("Test Footer", "testid");
-		assertTrue(s.header.get(2).contains("font-size=\"24"));
-		assertTrue(s.header.get(2).contains(">Test Footer<"));
-		assertTrue(s.header.get(2).contains("id=\"testid"));		
+		assertTrue(s.footer.get(1).contains("font-size=\"24"));
+		assertTrue(s.footer.get(1).contains(">Test Footer<"));
+		assertTrue(s.footer.get(1).contains("id=\"testid"));		
 	}
 
 	@Test
@@ -137,8 +137,8 @@ public class SVGWriterTest {
 	@Test
 	public void testPadSVG() {
 		SVGWriter svg = new SVGWriter(100, 100);
-		svg.xOffset = 50;
-		svg.yOffset = 50;
+		svg.xOffset = 37;
+		svg.yOffset = 37;
 		svg.padSVG();
 		// Assert there is a new SVG header with width 2 * xOffset + original width:
 		assertTrue(svg.header.get(1).contains("width=\"200"));
