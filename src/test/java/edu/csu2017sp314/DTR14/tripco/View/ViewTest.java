85152fd3e8ecd99e87d37f4c3e289c5771e637f8
bcgdwn <robot221@gmail.com>
2017-03-20 18:38:39 -0600

    Updates ViewTest

--- src/test/java/edu/csu2017sp314/DTR14/tripco/View/ViewTest.java
+++ src/test/java/edu/csu2017sp314/DTR14/tripco/View/ViewTest.java
@@ -16,11 +16,11 @@ public class ViewTest {
 	@Test
 	public void testConstructor() {
 		Presenter prez = new Presenter(new ArrayList<File>());
-		View v1 = new View(prez, "hello", "coloradoMap.svg", 300, false, false, false);
+		View v1 = new View("hello", "coloradoMap.svg", 300, false, false, false);
 		assertTrue("rootName not read correctly", v1.rootName.equals("hello"));
 		assertTrue("v1 flags do not match expected", v1.mileage == false && v1.ids == false && v1.names == false);
 		
-		View v2 = new View(prez, "hello.csv", "coloradoMap.svg", 300, true, true, true);
+		View v2 = new View("hello.csv", "coloradoMap.svg", 300, true, true, true);
 		assertTrue(".csv extension not properly removed from rootName", v2.rootName.equals("hello"));
 		assertTrue("v2 flags do not match expected", v2.mileage == true && v2.ids == true && v2.names == true);
 		assertTrue(v2.mileage == true);
@@ -32,15 +32,13 @@ public class ViewTest {
 	
 	@Test
 	public void testGetRootName() {
-		Presenter prez = new Presenter(new ArrayList<File>());
-		View v1 = new View(prez, "helloWorld.csv", "coloradoMap.svg", 300, false, false, false);
+		View v1 = new View("helloWorld.csv", "coloradoMap.svg", 300, false, false, false);
 		assertTrue("getRootName() method not returning correct root name", v1.getRootName().equals("helloWorld"));
 	}
 	
 	@Test
 	public void testDisplay() {
-		Presenter prez = new Presenter(new ArrayList<File>());
-		View v1 = new View(prez, "helloWorld.csv", "coloradoMap.svg", 300, false, false, false);
+		View v1 = new View("helloWorld.csv", "coloradoMap.svg", 300, false, false, false);
 		assertTrue(v1.display("hello").equals("hello"));
 	}
 	
@@ -49,68 +47,10 @@ public class ViewTest {
 	
 	@Test
 	public void testAddLeg() {
-		Presenter prez = new Presenter(new ArrayList<File>());
-		View v = new View(prez, "helloWorld.csv", "coloradoMap.svg", 300, true, false, false);
+		View v = new View("helloWorld.csv", "coloradoMap.svg", 300, true, false, false);
 		String test = v.addLeg(40, -108, "Not Denver", "id1", 39, -107, "Not CO Springs", "id2", 50);
 		assertTrue(v.legCount == 1);
 		assertTrue(test.equals("m"));
-	}
-	//Test getters for Selection
-	@Test
-	public void testGetCSV(){
-		View v = new View();
-		v.setSelection(new Selection("testSelect.xml"));
-		assertTrue(v.getCSV()==null);
-	}
-	@Test
-	public void testGetCSVName(){
-		View v = new View();
-		v.setSelection(new Selection("testSelect.xml"));
-		assertTrue(v.getCSV()==null);
-	}
-	@Test
-	public void testGetSelectFileName(){
-		View v = new View();
-		v.setSelection(new Selection("testSelect.xml"));
-		assertTrue(v.getSelectFilename().equalsIgnoreCase("testSelect.xml"));
-	}
-	@Test
-	public void testGetBackSVGName(){
-		View v = new View();
-		v.setSelection(new Selection("testSelect.xml"));
-		v.getSelect().setBackSVG("test");
-		assertTrue(v.getBackSVGName().equalsIgnoreCase("test"));
-	}
-	@Test
-	public void testGetOptions(){
-		View v = new View();
-		v.setSelection(new Selection("testSelect.xml"));
-		boolean[] t = new boolean[] {true, false, true};
-		v.getSelect().setOptions(t);
-		boolean [] ta = v.getOptions();
-		assertTrue(t[0]==ta[0]);
-		assertTrue(t[1]==ta[1]);
-		assertTrue(t[2]==ta[2]);
-	}
-	@Test
-	public void testGetOpts(){
-		View v = new View();
-		v.setSelection(new Selection("testSelect.xml"));
-		boolean[] t = new boolean[] {true, false};
-		v.getSelect().setOpt(t);
-		boolean [] ta = v.getOpts();
-		assertTrue(t[0]==ta[0]);
-		assertTrue(t[1]==ta[1]);
-	}
-	@Test
-	public void testGetSubset(){
-		View v = new View();
-		v.setSelection(new Selection("testSelect.xml"));
-		String[] t = new String[] {"1", "3"};
-		v.getSelect().setSubset(t);
-		String [] ta = v.getSubset();
-		assertTrue(t[0].equalsIgnoreCase(ta[0]));
-		assertTrue(t[1].equalsIgnoreCase(ta[1]));
 	}
 	
 
