2f28cade3e66f09e0431cda845f58700a8a6ff35
tjohns0n <tjohns0n@rams.colostate.edu>
2017-03-28 21:02:24 -0600

    fixes #219 Test classes that require files now write test files, run associated tests, and then delete the tests files.

--- src/test/java/edu/csu2017sp314/DTR14/tripco/Model/ModelTest.java
+++ src/test/java/edu/csu2017sp314/DTR14/tripco/Model/ModelTest.java
@@ -13,6 +13,28 @@ public class ModelTest{
 
 	String filename = "test.csv";
 
+	@BeforeClass
+	public static void writeTestFile() {
+		String testfile = "test.csv";
+		try {
+			BufferedWriter w = new BufferedWriter(new FileWriter(testfile));
+			w.write("name,id,Elevation,Estimated Prominence," 
+					+ "latitude,longitude,Quadrangle,Range" 
+					+ "\nMount Elbert,1,14433,9093,39.1177,"
+					+ "-106.4453,Mount Elbert,Sawatch"
+					+ "\nMount Massive,2,14421,1961,39.1875,"
+					+ "-106.4756,Mount Massive,Sawatch"
+					+ "\nMount Harvard,3,14420,2360,38.9243,"
+					+ "-106.3208,Mount Harvard,Sawatch"
+					+ "\nBlanca Peak,4,14345,5325,37.5774,"
+					+ "-105.4857,Blanca Peak,Sangre de Cristo)");
+			w.close();
+		} catch (IOException e) {
+			System.err.println("Failed to open new file for writing - ModelTest");
+			e.printStackTrace();
+		}
+	}
+	
 	@Test
 	public void testConstructor() {
 		Model model = new Model(filename);
@@ -33,5 +55,12 @@ public class ModelTest{
 		assertTrue("wrong trip", tests[0][0].equals("0,Mount Elbert,39.1177,-106.4453"));
 		assertTrue("wrong trip", tests[1][1].equals("2,14421,1961,Mount Massive,Sawatch"));
 		assertTrue("wrong trip", tests[2][2].equals("id,Elevation,Estimated Prominence,Quadrangle,Range"));
+	}
+	
+	@AfterClass
+	public static void removeTestFile() {
+		File f = null;
+		f = new File("test.csv");
+		f.delete();
 	}
 }
