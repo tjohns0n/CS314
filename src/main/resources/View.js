a967525a2685b79f0f401c6af9afe742e9633e1c
GrEedWish <GrEedWish_x@outlook.com>
2017-04-10 17:20:53 -0600

    New added
    New added

---
+++ src/main/resources/View.js
@@ -0,0 +1,6 @@
+function nameSVG() {
+	return '/Users/Jim/Desktop/cs314/project/DTR-14-master/src/main/resources/ColoradoCountySeats.svg';
+}
+function nameXML() {
+	return '/Users/Jim/Desktop/cs314/project/DTR-14-master/src/main/resources/ColoradoCountySeats.xml';
+}
