15aa4b877f6463a09a7d25d38e0c118178f74dc4
bcgdwn <robot221@gmail.com>
2017-04-10 19:48:43 -0600

    Fixes itin query

--- src/main/java/edu/csu2017sp314/DTR14/tripco/Model/Query.java
+++ src/main/java/edu/csu2017sp314/DTR14/tripco/Model/Query.java
@@ -296,12 +296,11 @@ public class Query {
 						
 						try { // iterate through the query results and print
 							String add="";
-							int j=0;
-							while (rs.next()){
-								add+=rs.getString(j+1)+",";
-								j++;
-							}
-							ret[i] = add.substring(0, add.length()-1);
+							rs.next();
+							add+=rs.getString(1)+","+rs.getString(2)+","+rs.getString(3)+","+rs.getString(4)+",";
+							add+=rs.getString(5)+","+rs.getString(6)+","+rs.getString(7)+","+rs.getString(8)+",";
+							add+=rs.getString(9)+","+rs.getString(10)+","+rs.getString(11)+","+rs.getString(12);
+							ret[i] = add;
 						} finally { rs.close(); }
 					}
 				} finally { st.close(); }
